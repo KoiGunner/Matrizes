7f3bafb982ed9353f8519c012b347bae6f63efb4
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2024-05-16 16:16:08 -0300

    Update Program.java
    Dando uma aprimorada no código local.

--- Program.java
+++ Program.java
@@ -17,22 +17,22 @@ public class Program{
     int n = sc.nextInt();
     int [][] mat = new int [n][n];
 
-    for(i = 0; i < n; i++){
-      for(j = 0; j < n; j++{
+    for(i = 0; i < mat.length; i++){
+      for(j = 0; j < mat[i].length; j++{
         mat [i][j] = sc.nextInt();
       }
     }
 
     System.out.printf("Main diagonal: ");
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i<mat.length; i++){
       System.out.print(mat[i][i]);
       /*a lógica funciona da seguinte maneira, como a diagonal da matriz ocasionalmente eram posições cujo qual números são iguais, imprimindo as posições dele, da a diagonal perfeita*/
     }
     System.out.println();
 
     int count = 0;
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j<n; j++{
+    for(int i = 0; i <mat.length; i++){
+      for(int j = 0; j<mat[i].length; j++{
         if(mat[i][j] < 0){
           count++;
         }
